1b3ed60b27505f71cc3065791bd389fd58fdc565
AlixBen <alessb@hotmail.fr>
2021-03-27 22:56:57 +0100

    commit dice et test

---
+++ src/dice.py
@@ -0,0 +1,31 @@
+"""Make dice generator."""
+import random
+
+
+def main():
+    print("start game")
+    type_of_dice_flag = False
+    number_of_error = 0
+    while type_of_dice_flag is False:
+        type_of_dice = int(input("dice choice 6, 12, 24 :\n"))
+        if (type_of_dice == 6) or (type_of_dice == 12) or (type_of_dice == 24):
+            type_of_dice_flag = True
+        else:
+            number_of_error = number_of_error + 1
+            print(f"try again bro : number_of_error {number_of_error}")
+            if number_of_error == 5:
+                return 1
+    dice_value = dice_roll(type_of_dice)
+    print_value(dice_value)
+    print("end game")
+
+
+def dice_roll(type_of_dice: int):
+    """ Roll the dice. """
+    dice_value = random.randint(1, type_of_dice)
+    return dice_value
+
+
+def print_value(dice_value: int):
+    """Printing the dice value."""
+    print(f"dice_value: {dice_value}")
---
+++ test/unit_test/test_dice.py
@@ -0,0 +1,37 @@
+"""Make test for function dice."""
+from unittest import mock
+
+from src import dice
+
+
+@mock.patch("src.dice.dice_roll")
+@mock.patch("src.dice.print_value")
+def test_main_good_choice(mock_print_value, mock_dice_roll, monkeypatch):
+    """Test main."""
+    monkeypatch.setattr("builtins.input", lambda _: "6")
+    mock_dice_roll.return_value = 5
+    result = dice.main()
+    assert result is None
+
+
+def test_main_bad_choice(monkeypatch):
+    """Test main."""
+    monkeypatch.setattr("builtins.input", lambda _: "5")
+    result = dice.main()
+    assert result == 1
+
+
+def test_print_value():
+    """test final print value."""
+    mock_dice_value = 6
+    result = dice.print_value(mock_dice_value)
+    assert result is None
+
+
+@mock.patch("src.dice.random")
+def test_dice_roll(mock_random):
+    """test function dice_roll."""
+    mock_type_of_dice = 6
+    mock_random.randint.return_value = 5
+    result = dice.dice_roll(mock_type_of_dice)
+    assert result == 5
